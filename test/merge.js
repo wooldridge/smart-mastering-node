0dcf7b1ff380e51d939665be0d71161c7194d600
Mike Wooldridge <mike@hellion.org>
2018-12-20 09:10:43 -0800

    Fix merge and its tests

--- test/merge.js
+++ test/merge.js
@@ -1,5 +1,5 @@
-const config = require('../config'),
-      chai = require('chai'),
+const chai = require('chai'),
+      moment = require('moment'),
       sm = require('../lib/sm'),
       fs = require('fs');
 
@@ -15,60 +15,187 @@ let client = sm.createClient({
   password: 'admin'
 });
 
-const testOptionsXML = fs.readFileSync(config.path + 'test/data/options/org-merge-options.xml').toString('utf8');
+let props = [['foo1', 'bar1'],['foo2', 'bar2']];
 
-const path = config.path + 'test/data/documents/json/';
-const docs = [
-  {
-    uri: 'org1.json',
-    content: fs.readFileSync(path + 'org1.json'),
-    collections: ['mdm-content', 'Source1']
-  },
-  {
-    uri: 'orgA.json',
-    content: fs.readFileSync(path + 'orgA.json'),
-    collections: ['mdm-content', 'Source2']
+let docs = props.map(function(p, i) {
+  let content = {
+    envelope: {
+      instance: {
+        test: {
+            prop1: p[0],
+            prop2: p[1],
+            prop3: p[2]
+        },
+        info: { title: 'test', version: '0.0.1' }
+      },
+      headers: {
+        id: i,
+        sources: [
+          {
+            name: 'source' + (i + 1),
+            dateTime: moment().add(i, 'days').format(),
+            user: "mdm-admin"
+          }
+      ]
+      },
+      triples: [],
+      attachments: {
+        test: {
+            prop1: p[0],
+            prop2: p[1],
+            prop3: p[2]
+        }
+      }
+    }
+  };
+  return {
+    uri: '/merge-doc' + (i+1) + '.json',
+    collections: ['mdm-content', 'source' + (i + 1)],
+    content: content
+  };
+});
+
+let optionsJSON = {
+  options: {
+    propertyDefs: {
+      properties: [
+        { namespace: "", localname: "prop1", name: "prop1" },
+        { namespace: "", localname: "prop2", name: "prop2" }
+      ]
+    },
+    merging: [
+      {
+        propertyName: "prop1",
+        maxValues: "1",
+        sourceWeights: [
+          { source: { name: "source1", weight: "10" } },
+          { source: { name: "source2", weight: "100" } }
+        ]
+      },
+      {
+        propertyName: "prop2",
+        maxValues: "1",
+        sourceWeights: [
+          { source: { name: "source1", weight: "100" } },
+          { source: { name: "source2", weight: "10" } }
+        ]
+      }
+    ]
   }
-]
+};
+optionsJSON = JSON.stringify(optionsJSON);
+
+let optionsXML = `<?xml version="1.0" encoding="UTF-8"?>
+<options xmlns="http://marklogic.com/smart-mastering/merging">
+  <property-defs>
+    <property namespace="" localname="prop1" name="prop1"/>
+    <property namespace="" localname="prop2" name="prop2"/>
+  </property-defs>
+  <merging>
+    <merge property-name="prop1" max-values="1">
+      <source-weights>
+        <source name="source1" weight="100"/>
+        <source name="source2" weight="10"/>
+      </source-weights>
+    </merge>
+    <merge property-name="prop2" max-values="1">
+      <source-weights>
+        <source name="source1" weight="10"/>
+        <source name="source2" weight="100"/>
+      </source-weights>
+    </merge>
+  </merging>
+</options>
+`;
 
 before((done) => {
   client.mlClient.documents.write(docs)
   .result((res) => {
-    return client.mergeOptions.write('org-merge-options', testOptionsXML);
+    return client.mergeOptions.write('merging-options-json', optionsJSON);
+  })
+  .then((res) => {
+    return client.mergeOptions.write('merging-options-xml', optionsXML);
   })
   .then((res) => {
     done();
   });
 });
 
-// after((done) => {
-//   client.mlClient.documents.remove(['doc1.json', 'doc2.json'])
-//   .result((res) => {
-//     return client.mergeOptions.remove('merge-options2');
-//   })
-//   .then((res) => {
-//     done();
-//   });
-// });
+after((done) => {
+  client.mlClient.documents.remove(['/merge-doc1.json', '/merge-doc2.json'])
+  .result((res) => {
+    return client.mergeOptions.remove('merging-options-json');
+  })
+  .then((res) => {
+    return client.mergeOptions.remove('merging-options-xml');
+  })
+  .then((res) => {
+    return client.mlClient.documents.removeAll({collection: 'mdm-auditing'});
+  })
+  .then((res) => {
+    return client.mlClient.documents.removeAll({collection: 'mdm-merged'});
+  })
+  .then((res) => {
+    done();
+  });
+});
+
+let uriToRestore = '';
 
 describe('Merge', () => {
-  xit('should be run with options ref', () => {
+  it('should be run with options ref JSON previewed', () => {
     let options = {
-      uris: ['org1.json', 'orgA.json'],
-      optionsName: 'org-merge-options',
+      uris: ['/merge-doc1.json', '/merge-doc2.json'],
+      optionsName: 'merging-options-json',
       preview: true
     }
     return client.merge.run(options)
     .then((res) => {
-      //console.log(res);
-      // assert.isNotEmpty(res.body.results);
+      let srcs = res.envelope.headers.sources;
+      assert.equal(srcs[0].name, 'source1');
+      let inst = res.envelope.instance;
+      assert.equal(inst.test.prop1, 'foo2');
+      assert.equal(inst.test.prop2, 'bar1');
     })
   });
-  xit('should be restored', () => {
-    return client.match.restore('docA.xml', true)
+  xit('should be run with options content JSON previewed', () => {
+    console.log('optionsJSON', optionsJSON);
+    let options = {
+      uris: ['/merge-doc1.json', '/merge-doc2.json'],
+      mergeOptions: optionsJSON,
+      preview: true
+    }
+    return client.merge.run(options)
     .then((res) => {
-      console.log(res.body);
-      // assert.isNotEmpty(res.body.results);
+      console.log(res);
+      let srcs = res.envelope.headers.sources;
+      assert.equal(srcs[0].name, 'source1');
+      let inst = res.envelope.instance;
+      assert.equal(inst.test.prop1, 'foo2');
+      assert.equal(inst.test.prop2, 'bar1');
+    })
+  });
+  it('should be run with options ref XML merged', () => {
+    let options = {
+      uris: ['/merge-doc1.json', '/merge-doc2.json'],
+      optionsName: 'merging-options-xml',
+      preview: false
+    }
+    return client.merge.run(options)
+    .then((res) => {
+      let srcs = res.envelope.headers.sources;
+      assert.equal(srcs[0].name, 'source1');
+      let inst = res.envelope.instance;
+      assert.equal(inst.test.prop1, 'foo1');
+      assert.equal(inst.test.prop2, 'bar2');
+      uriToRestore = res.envelope.headers.id;
+    })
+  });
+  // The following returns success but doesn't seem to do anything
+  xit('should be restored', () => {
+    return client.merge.restore(uriToRestore, false)
+    .then((res) => {
+      console.log(res);
     })
   });
 });
